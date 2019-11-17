54e6c86276280b7278cd89b96813c47472f8882e
huy <dinhhuyams@gmail.com>
2019-11-17 14:26:29 +0200

    feat: add project section

--- src/screens/projects/Projects.tsx
+++ src/screens/projects/Projects.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import {RouteComponentProps} from '@reach/router'
+import Project from './components/Project'
+import Banner from '../../components/Banner/Banner'
+import BannerImage from '../../assets/banner.jpg'
+import {ProjectListWrapper} from './style'
 
 interface Props extends RouteComponentProps {}
 
+const mockHomeBanner = {
+	content:
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales eget leo et dignissim. Proin tincidunt lacus lacinia turpis gravida hendrerit',
+	imgUrl: BannerImage,
+}
+
 const Projects: React.FC<Props> = () => {
-	return <div>Project page!</div>
+	return (
+		<>
+			<Banner text={mockHomeBanner.content} imageUrl={mockHomeBanner.imgUrl} />
+			<ProjectListWrapper>
+				<Project />
+				<Project />
+				<Project />
+				<Project />
+			</ProjectListWrapper>
+		</>
+	)
 }
 
 export default Projects
