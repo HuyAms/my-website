255e677c57273d37812310b03bf2a536cba2bd53
huy <dinhhuyams@gmail.com>
2019-11-23 18:46:43 +0200

    feat: add scroll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,15 @@ import Projects from './screens/projects/Projects'
 import Blogs from './screens/blogs/Blogs'
 import {Router} from '@reach/router'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import IconUp from './assets/arrow-top.svg'
+import {BackToTop} from './style'
+import {animateScroll} from 'react-scroll'
 
 const App: React.FC = () => {
+	const scrollToTop = () => {
+		animateScroll.scrollToTop()
+	}
+
 	return (
 		<AppLayout>
 			<Router primary={false}>
@@ -17,6 +24,7 @@ const App: React.FC = () => {
 					<Blogs path="/blogs" />
 				</ScrollToTop>
 			</Router>
+			<BackToTop onClick={scrollToTop} src={IconUp} />
 		</AppLayout>
 	)
 }
--- src/style.ts
+++ src/style.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
-	background-color: red;
+export const BackToTop = styled.img`
+	position: fixed;
+	bottom: 2.4rem;
+	right: 2.4rem;
+	width: 4.8rem;
+	height: 4.8rem;
+	object-fit: contain;
+	cursor: pointer;
 `
